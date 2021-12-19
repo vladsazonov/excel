68b814bcdfcf39290dfae810fbcd432d66893bf3
v.sazonov <v.sazonov@elonsoft.ru>
2021-12-19 14:06:32 +0300

    Add table resize

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,10 +1,46 @@
+import { $ } from '../../core/dom';
 import { ExcelComponent } from '../../core/ExcelComponent';
 import { createTable } from './table.template';
 
 export class Table extends ExcelComponent {
   static className = "excel__table"
 
+  constructor($root) {
+    super($root, {
+      listeners: ["mousedown"],
+    })
+  }
+
   toHTML() {
     return createTable()
+  }
+
+  onMousedown(event) {
+    if (event.target.dataset.resize) {
+      const $resizer = $(event.target)
+      const $parent = $resizer.closest('[data-type="resizable"]')
+      const coords = $parent.getCoords()
+      const cells = this.$root.findAll(`[data-col="${$parent.data.col}"]`)
+      const type = $resizer.data.resize
+
+      document.onmousemove = (e) => {
+        if (type === "col") {
+          const delta = e.pageX - coords.right
+          const value = coords.width + delta
+
+          $parent.css({ width: value + "px" })
+          cells.forEach((el) => (el.style.width = value + "px"))
+        } else {
+          const delta = e.pageY - coords.bottom
+          const value = coords.height + delta
+
+          $parent.css({ height: value + "px" })
+        }
+      }
+
+      document.onmouseup = () => {
+        document.onmousemove = null
+      }
+    }
   }
 }
